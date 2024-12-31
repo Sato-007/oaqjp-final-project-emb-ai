78343b4b19158aa260a9b5afaf042005b5079e07
Sato-007 <satyaki.bose3@gmail.com>
2024-12-31 09:00:14 -0500

    function and unit test

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,4 +9,42 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        emotion_data = formatted_response['emotionPredictions'][0]['emotion']
+
+        anger_score = emotion_data['anger']
+        disgust_score = emotion_data['disgust']
+        fear_score = emotion_data['fear']
+        joy_score = emotion_data['joy']
+        sadness_score = emotion_data['sadness']
+
+        emotion_scores = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+        output = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
+    elif response.status_code == 500:
+        output = {
+            'anger': none,
+            'disgust': none,
+            'fear': none,
+            'joy': none,
+            'sadness': none,
+            'dominant_emotion': none
+        }
+    
+    return output
